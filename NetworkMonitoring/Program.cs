f5f9d3c77103a14b1a2e38212ff0d5264592c7cf
Levente Kis <kislevi94@gmail.com>
2018-02-11 11:56:09 +0100

    Add project files.

---
+++ NetworkMonitoring/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkMonitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("alma");
+            Console.ReadKey();
+        }
+    }
+}
